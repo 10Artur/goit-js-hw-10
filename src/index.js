1bdb2d9d68bb60001a370c317ddd87d85abd81cc
10Artur <artyr11012002@gmail.com>
2023-07-19 09:46:25 +0300

    update

---
+++ src/index.js
@@ -0,0 +1,104 @@
+import axios from 'axios';
+import SlimSelect from 'slim-select';
+import Notiflix from 'notiflix';
+import API from './cat-api';
+
+const API_KEY =
+    'live_bvPUqrWOT8Z9DgXgZnTWdVdqe3sWFdgkS0hBkbyUTOm1BSAUnuZhETMnPiGDcgr1';
+
+axios.defaults.headers.common['x-api-key'] = API_KEY;
+
+const selectElement = document.querySelector('.breed-select');
+const selectContainer = document.querySelector('.breed-select-container'); // додано
+const loaderElement = document.querySelector('.loader');
+const errorElement = document.querySelector('.error');
+const catInfoElement = document.querySelector('.cat-info');
+
+function populateBreeds(breeds) {
+    selectElement.innerHTML = breeds
+        .map(breed => {
+            return `<option value="${breed.id}">${breed.name}</option>`;
+        })
+        .join('');
+}
+
+function showLoader() {
+    loaderElement.style.display = 'block';
+}
+
+function hideLoader() {
+    loaderElement.style.display = 'none';
+}
+
+function showError() {
+    errorElement.style.display = 'block';
+}
+
+function hideError() {
+    errorElement.style.display = 'none';
+}
+
+function showCatInfo(catInfo) {
+    catInfoElement.innerHTML = `
+      <img src="${catInfo.url}" alt="Cat Image" />
+      <p>Breed: ${catInfo.breed}</p>
+      <p>Description: ${catInfo.description}</p>
+      <p>Temperament: ${catInfo.temperament}</p>
+    `;
+    catInfoElement.style.display = 'block';
+}
+
+function hideCatInfo() {
+    catInfoElement.style.display = 'none';
+}
+
+async function fetchBreeds() {
+    try {
+        const response = await axios.get('https://api.thecatapi.com/v1/breeds');
+        const breeds = response.data;
+        populateBreeds(breeds);
+        selectContainer.style.display = 'block';
+    } catch (error) {
+        console.error(error);
+        showError();
+    } finally {
+        hideLoader();
+    }
+}
+
+async function fetchCatByBreed(breedId) {
+    try {
+        const response = await axios.get(
+            `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`
+        );
+        const catInfo = {
+            url: response.data[0].url,
+            breed: response.data[0].breeds[0].name,
+            description: response.data[0].breeds[0].description,
+            temperament: response.data[0].breeds[0].temperament,
+        };
+        showCatInfo(catInfo);
+    } catch (error) {
+        console.error(error);
+        showError();
+    } finally {
+        hideLoader();
+    }
+}
+
+selectElement.addEventListener('change', function() {
+    const selectedBreedId = this.value;
+    if (selectedBreedId) {
+        showLoader();
+        hideError();
+        hideCatInfo();
+        fetchCatByBreed(selectedBreedId);
+    } else {
+        hideCatInfo();
+    }
+});
+
+selectContainer.style.display = 'none';
+showLoader();
+
+fetchBreeds();
